418451025a994bd77f3fe202900637dbb4dd6e1b
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2024-04-01 12:00:10 +0300

    update docker files

--- crm-master-profile/build.sh
+++ crm-master-profile/build.sh
@@ -3,4 +3,4 @@ docker stop crm-front
 docker rm --force front
 docker rmi front-image
 docker build . -t front-image
-docker run -d -p 80:80 --name front front-image
+docker run -d -p 8080:80 --name front front-image
