b93c7082e88dad3d1c9cdb7d6b957a5157f5f8e8
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2024-04-01 20:29:28 +0300

    fix docker

--- crm-master-profile/build.sh
+++ crm-master-profile/build.sh
@@ -3,4 +3,4 @@ docker stop crm-front
 docker rm --force crm-front
 docker rmi crm-front-image
 docker build . -t crm-front-image
-docker run -d -p 80:80 --name crm-front crm-front-image
+docker run -d -p 3000:80 --name crm-front crm-front-image
